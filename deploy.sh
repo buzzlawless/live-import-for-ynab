caf0522d7c7e839b49d2f62412baa02156228331
Morgan Lawless <mlawless@protonmail.com>
2022-10-16 12:41:43 -0400

    Use budget ID instead of random string to make uniquely named S3 bucket for cloudformation packaging

--- deploy.sh
+++ deploy.sh
@@ -49,8 +49,7 @@ fi
 
 # Create AWS resources
 
-BUCKET_ID=$(dd if=/dev/random bs=8 count=1 2>/dev/null | od -An -tx1 | tr -d ' \t\n')
-BUCKET_NAME=live-import-for-ynab-lambda-artifacts-"$BUCKET_ID"
+BUCKET_NAME=live-import-for-ynab-lambda-artifacts-"$BUDGET_ID"
 aws s3 mb s3://"$BUCKET_NAME"
 echo "Created S3 bucket for lambda artifacts: $BUCKET_NAME"
 
